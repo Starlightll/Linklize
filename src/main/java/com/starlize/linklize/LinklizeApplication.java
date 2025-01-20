ce3a0ac7eed9931683b3e820aa3ab48acd61750c
NGUYEN HOANG LONG <145125551+Starlightll@users.noreply.github.com>
2025-01-21 01:08:52 +0700

    First Commit

---
+++ src/main/java/com/starlize/linklize/LinklizeApplication.java
@@ -0,0 +1,13 @@
+package com.starlize.linklize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinklizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinklizeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/starlize/linklize/LinklizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starlize.linklize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinklizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
